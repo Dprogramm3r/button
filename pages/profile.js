60b6fede9dce6b94bce5a9902396a1ceeec06a16
Dprogramm3r <32206379+Dprogramm3r@users.noreply.github.com>
2023-01-07 12:56:32 +0100

    refactored

--- pages/profile.js
+++ pages/profile.js
@@ -12,7 +12,7 @@ import {
 
 
  import { BigNumber, ethers } from 'ethers'
-
+import * as Image from 'next/image';
  import * as React from 'react'
  
  export function Profile() {
@@ -45,22 +45,22 @@ import {
       },
     })
     const data = await sendTransaction(config)
-    const isSuccess = useWaitForTransaction({
-      hash: data?.hash,
-    })
-    setMessage(isSuccess);
+    // const isSuccess = useWaitForTransaction({
+    //   hash: data?.hash,
+    // })
+    setMessage(await data.wait);
   }
   return(  
     <> 
 <nav className="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded white:bg-blue-900">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="#" className="flex items-center">
-        <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
+        <Image src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
     </a>
     <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
-      <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+      <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
       <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-black dark:bg-blue-800 md:dark:bg-gray-900 dark:border-gray-700">
