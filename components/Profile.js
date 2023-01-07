6f5e7f762232cffa9cad0abda3ed3eedd9ed6e43
Dprogramm3r <32206379+Dprogramm3r@users.noreply.github.com>
2023-01-07 16:54:32 +0100

    update

--- components/Profile.js
+++ components/Profile.js
@@ -60,7 +60,7 @@ import {
         <div>
           <a href="#" className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900">
             <svg className="h-6 w-6 mr-1 text-blue-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
             </svg>
             <span className="font-bold">Solidity</span>
           </a>
@@ -102,7 +102,7 @@ import {
       <div className="md:hidden flex items-center">
         <button className="mobile-menu-button">
           <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
           </svg>
         </button>
       </div>
