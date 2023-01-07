224fca89d545d1126bc9018e0ec4c480c78383ca
Dprogramm3r <32206379+Dprogramm3r@users.noreply.github.com>
2023-01-07 14:18:40 +0100

    entry

--- components/profile.js
+++ components/profile.js
@@ -4,13 +4,11 @@ import {
   useDisconnect,
   useEnsAvatar,
   useEnsName,
-  usePrepareSendTransaction,
-  useSendTransaction,
-  useWaitForTransaction
- } from 'wagmi'
- import { fetchBalance,sendTransaction,prepareSendTransaction } from '@wagmi/core'
-
-
+  fetchBalance,
+  sendTransaction,
+  prepareSendTransaction 
+ } from 'wagmi';
+//  import { fetchBalance,sendTransaction,prepareSendTransaction } from '@wagmi/core'
  import { BigNumber, ethers } from 'ethers'
  import * as React from 'react'
  
@@ -52,7 +50,7 @@ import {
   }
   return(  
     <> 
-<nav className="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded white:bg-blue-900">
+   <nav className="bg-black border-gray-200 px-2 sm:px-4 py-2.5 rounded white:bg-blue-900">
   <div className="container flex flex-wrap items-center justify-between mx-auto">
     <a href="#" className="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
@@ -63,7 +61,7 @@ import {
       <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
     </button>
     <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-black dark:bg-blue-800 md:dark:bg-gray-900 dark:border-gray-700">
+      <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-blue-800 md:dark:bg-gray-900 dark:border-gray-700">
     
       { isConnected && (
         <>    
