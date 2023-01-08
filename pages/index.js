acda4844282673a028360ab77886ebf4a4394f41
Dprogramm3r <32206379+Dprogramm3r@users.noreply.github.com>
2023-01-08 10:19:22 +0100

    refactored

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import  Profile from '../components/Profile'
+import "@rainbow-me/rainbowkit/styles.css";
 
 import { WagmiConfig, createClient, configureChains  } from 'wagmi'
 import { avalanche, bsc,optimism, arbitrum, mainnet,polygon, localhost } from '@wagmi/core/chains'
@@ -10,6 +11,15 @@ import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
+import {
+  RainbowKitProvider,connectorsForWallets, darkTheme
+} from '@rainbow-me/rainbowkit';
+import {
+  injectedWallet,
+  rainbowWallet,
+  walletConnectWallet,metaMaskWallet,trustWallet,coinbaseWallet,omniWallet,ledgerWallet,braveWallet 
+} from '@rainbow-me/rainbowkit/wallets';
+
 
 // Configure chains & providers with the Alchemy provider.
 // Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
@@ -18,26 +28,34 @@ const { chains, provider, webSocketProvider } = configureChains(
   [publicProvider()],
 )
  
+const connectors = connectorsForWallets([
+  {
+    groupName: 'Recommended',
+    wallets: [
+      // injectedWallet({ chains }),
+      metaMaskWallet({ chains }),
+      trustWallet({ chains }),
+      walletConnectWallet({ chains }),
+      rainbowWallet({ chains }),
+
+    ],
+  },
+  {
+    groupName: 'More',
+    wallets: [
+      coinbaseWallet({ chains, appName: 'Others' }),
+      ledgerWallet({ chains }),
+      braveWallet({ chains }),
+      omniWallet({ chains }),
+    ],
+  },
+]);
+
+
 // Set up client
 const client = createClient({
   // autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({chains
-    }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'multiChain Connect',
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-   
-  ],
+  connectors: connectors ,
   provider,
   webSocketProvider,
 })
@@ -45,7 +63,9 @@ const client = createClient({
 export default function Home() {
   return (
     <WagmiConfig client={client}>
+    <RainbowKitProvider theme={darkTheme()} chains={chains}  modalSize="compact" >
       <Profile />
+      </RainbowKitProvider>
     </WagmiConfig>
   )
 }
