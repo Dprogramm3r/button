b3185692d98678970735d6aab5dfdb9cd41e242e
Dprogramm3r <32206379+Dprogramm3r@users.noreply.github.com>
2023-01-10 09:50:50 +0100

    removed

--- components/Profile.js
+++ components/Profile.js
@@ -75,7 +75,7 @@ import {
   <>
   {connectors.map((connector) => (
         <a key={connector.id}>
-        <button disabled={!connector.ready} onClick={() => connect({ connector })}
+        <button onClick={() => connect({ connector })}
    className="py-2 px-3 bg-yellow-400 hover:bg-yellow-300 text-yellow-900 hover:text-yellow-800 rounded transition duration-300">{connector.name}
    </button>
   {!connector.ready && '(unsupported)'}
